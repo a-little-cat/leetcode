64eaa7c796064e1e401cc03f645ed4cb4388962f
cat <83169158@qq.com>
2022-08-26 21:26:46 +0800

    update twosum double point

--- leetcode/inc/Solution.h
+++ leetcode/inc/Solution.h
@@ -1,7 +1,9 @@
 #include <vector>
+using std::vector;
 
 class Solution
 {
   public:
     static int test(int input);
+    static vector<int> twoSum(vector<int>& numbers, int target);
 };
--- leetcode/src/Solution.cpp
+++ leetcode/src/Solution.cpp
@@ -4,3 +4,18 @@ int Solution::test(int input)
 {
     return input + 1;
 }
+
+vector<int> Solution::twoSum(vector<int>& numbers, int target)
+{
+    int sum = 0;
+    int left = 0;
+    int right = static_cast<int>(numbers.size() - 1);
+    while (numbers[left] + numbers[right] != target)
+    {
+        if (numbers[left] + numbers[right] > target)
+            right--;
+        else
+            left++;
+    }
+    return {left + 1, right + 1};
+}
